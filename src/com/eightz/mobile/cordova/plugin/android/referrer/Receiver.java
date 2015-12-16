4e0806e2f5312881e1c95f4fe38557168fe6d4df
podliy16 <podliy16@gmail.com>
2015-12-16 12:09:58 +0300

    Update Receiver.java

--- src/com/eightz/mobile/cordova/plugin/android/referrer/Receiver.java
+++ src/com/eightz/mobile/cordova/plugin/android/referrer/Receiver.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
-import com.google.analytics.tracking.android.CampaignTrackingReceiver;
+import com.google.android.gms.analytics.CampaignTrackingReceiver;
 
 import android.util.Log;
 
